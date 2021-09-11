b6f4a83720e7ff3aa4fc602b8a3923b021483446
sidalto <sidalto.pj@gmail.com>
2021-09-10 21:19:17 -0300

    teste do servidor

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  console.log('teste');
+});
+
 app.listen(3333, () => console.log('Server is running!'));
